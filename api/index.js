99d0c37b318a099ed99e8a0ac0555a6497c434f5
Jaures <jauresbeinajmin@gmail.com>
2022-07-13 23:21:18 +0100

    Fonction connection avec mongo en ligne

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from 'express';
-
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
 const app=express()
+dotenv.config()
+
+const connect = async
+try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    handleError(error);
+  }
+
+
+
+
+
 app.listen(8800, ()=>{
 console.log ("connexion backend")
 })
