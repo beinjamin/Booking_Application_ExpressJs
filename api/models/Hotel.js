e4c3cf9c41d4ae21eb62bc5c3e2f021ef688258e
Jaures <jauresbeinajmin@gmail.com>
2022-07-15 21:02:47 +0100

    Mise en place du model hotel

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -11,5 +11,63 @@ const HotelSchema = new mongoosescema.Schema({
         type: String,
         require: true
 
-    }
+    },
+    city:{
+        type: String,
+        require: true
+
+    },
+    type:{
+        type: String,
+        require: true
+
+    },
+    adress:{
+        type: String,
+        require: true
+        
+    },
+    distance:{
+        type: String,
+        require:true
+    },
+    photos:{
+        type: String,
+
+    },
+    city:{
+        type: String,
+        require: true
+
+    },
+    type:{
+        type: String,
+        require: true
+
+    },
+    adress:{
+        type: String,
+        require: true
+        
+    },
+
+    distance:{
+        type: String,
+        required:true
+    },
+    photos:{
+        type: [String],
+    },
+    desc:{
+        type: String,
+        required: true
+    },
+    rating:{
+        type: Number,
+        min:0,
+        max:5
+    },
+    
+    
+    
 })
